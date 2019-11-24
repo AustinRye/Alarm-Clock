b84ba80573e6bf2b43c7525a7db7eac5e89697cd
AustinWRye <RyeAustinW@gmail.com>
2019-11-23 21:30:15 -0500

    Add Master Controller FSMs and Reset display

--- src/hdl/Master_Controller.v
+++ src/hdl/Master_Controller.v
@@ -7,33 +7,155 @@
 module Master_Controller
     (
         input i_Clk,
+        input i_Reset,
         input i_Change_Time,
         input i_Change_Alarm,
         input i_Hours_Inc,
         input i_Minutes_Inc,
         input i_Alarm_Enable,
-        input [15:0] i_Time,
-        input [15:0] i_Alarm_Time,
+        input [31:0] i_Time,
+        input [31:0] i_Alarm_Time,
+        output o_Time_Minutes_Inc,
+        output o_Time_Hours_Inc,
+        output o_Alarm_Minutes_Inc,
+        output o_Alarm_Hours_Inc,
         output o_Display_Sel,
         output o_Alarm_On,
         output o_Alarm_Enabled
     );
     
-    reg r_Display_Sel = 0;
-    reg r_Alarm_On;
+    //////////////////////////////////////////////////////////////////////////////
+    // Time FSM
+    //////////////////////////////////////////////////////////////////////////////
     
-    always @(posedge i_Clk) begin
-        if (~i_Change_Time & i_Change_Alarm)
-            r_Display_Sel <= 1;
+    reg r_Display_Sel;
+    reg r_Time_Minutes_Inc;
+    reg r_Time_Hours_Inc;
+    reg r_Alarm_Minutes_Inc;
+    reg r_Alarm_Hours_Inc;
+    
+    reg [1:0] r_Time_State, r_Time_Next_State;
+    parameter RESET = 0, RUN = 1, SET = 2;
+    
+    always @(posedge i_Clk or posedge i_Reset) begin
+        if (i_Reset)
+            r_Time_State <= RESET;
         else
-            r_Display_Sel <= 0;
-            
-        if (i_Alarm_Enable & (r_Alarm_On | (i_Time == i_Alarm_Time)))
-            r_Alarm_On <= 1;
-        else
-            r_Alarm_On <= 0;
+            r_Time_State <= r_Time_Next_State;
     end
     
+    always @(r_Time_State or i_Change_Time or i_Change_Alarm) begin
+        case (r_Time_State)
+            RESET:
+                begin
+                    if (i_Reset)
+                        r_Time_Next_State <= RESET;
+                    else if (i_Change_Time | i_Change_Alarm)
+                        r_Time_Next_State <= SET;
+                    else
+                        r_Time_Next_State <= RUN;
+                end
+            RUN:
+                begin
+                    if (i_Change_Time | i_Change_Alarm)
+                        r_Time_Next_State <= SET;
+                    else
+                        r_Time_Next_State <= RUN;
+                end
+            SET:
+                begin
+                    if (i_Change_Time | i_Change_Alarm)
+                        r_Time_Next_State <= SET;
+                    else
+                        r_Time_Next_State <= RUN;
+                end
+            default:
+                begin
+                    r_Time_Next_State <= RUN;
+                end
+        endcase
+    end
+    
+    always @(r_Time_State) begin
+        case (r_Time_State)
+            SET:
+                begin
+                    r_Display_Sel <= (~i_Change_Time & i_Change_Alarm) ? 1 : 0;
+                    r_Time_Minutes_Inc <= (i_Change_Time & i_Minutes_Inc) ? 1 : 0;
+                    r_Time_Hours_Inc <= (i_Change_Time & i_Hours_Inc) ? 1 : 0;
+                    r_Alarm_Minutes_Inc <= (i_Change_Alarm & i_Minutes_Inc) ? 1 : 0;
+                    r_Alarm_Hours_Inc <= (i_Change_Alarm & i_Hours_Inc) ? 1 : 0;
+                end
+            default:
+                begin
+                    r_Time_Minutes_Inc <= 0;
+                    r_Time_Hours_Inc <= 0;
+                    r_Alarm_Minutes_Inc <= 0;
+                    r_Alarm_Hours_Inc <= 0;
+                end
+        endcase
+    end
+    
+//    //////////////////////////////////////////////////////////////////////////////
+//    // Alarm FSM
+//    //////////////////////////////////////////////////////////////////////////////
+    
+    reg r_Alarm_On;
+    
+    reg r_Alarm_State, r_Alarm_Next_State;
+    parameter OFF = 0, ON = 1;
+    
+    always @(posedge i_Clk or posedge i_Reset) begin
+        if (i_Reset)
+            r_Alarm_State <= OFF;
+        else
+            r_Alarm_State <= r_Alarm_Next_State;
+    end
+    
+    always @(r_Alarm_State or i_Alarm_Enable) begin
+        case (r_Alarm_State)
+            OFF:
+                begin
+                    if (~i_Reset & i_Alarm_Enable & (i_Time == i_Alarm_Time))
+                        r_Alarm_Next_State <= ON;
+                    else
+                        r_Alarm_Next_State <= OFF;
+                end
+            ON:
+                begin
+                    if (i_Reset | ~i_Alarm_Enable)
+                        r_Alarm_Next_State <= OFF;
+                    else
+                        r_Alarm_Next_State <= ON;
+                end
+            default:
+                begin
+                    r_Alarm_Next_State <= OFF;
+                end
+        endcase
+    end
+    
+    always @(r_Alarm_State) begin
+        case (r_Alarm_State)
+            OFF:
+                begin
+                    r_Alarm_On <= 0;
+                end
+            ON:
+                begin
+                    r_Alarm_On <= 1;
+                end
+            default:
+                begin
+                    r_Alarm_On <= 0;
+                end
+        endcase
+    end
+    
+    assign o_Time_Minutes_Inc = r_Time_Minutes_Inc;
+    assign o_Time_Hours_Inc = r_Time_Hours_Inc;
+    assign o_Alarm_Minutes_Inc = r_Alarm_Minutes_Inc;
+    assign o_Alarm_Hours_Inc = r_Alarm_Hours_Inc;
     assign o_Display_Sel = r_Display_Sel;
     assign o_Alarm_On = r_Alarm_On;
     assign o_Alarm_Enabled = i_Alarm_Enable;
--- src/hdl/Seven_Segment_Display_Refresh.v
+++ src/hdl/Seven_Segment_Display_Refresh.v
@@ -10,6 +10,7 @@ module Seven_Segment_Display_Refresh
         parameter                SEGMENT_NUM_USED = 1
     ) (
         input                     i_Refresh_Clk,
+        input                     i_Reset,
         input [4*SEGMENT_NUM_USED-1:0] i_BCD_Num,
         output [3:0]              o_BCD_Num_Sel,
         output [SEGMENT_NUM-1:0]  o_Anodes
@@ -25,10 +26,12 @@ module Seven_Segment_Display_Refresh
         else
             r_Segment_Count <= r_Segment_Count + 1;
         
-        if (r_Segment_Count <= SEGMENT_NUM_USED - 1)
+        if (i_Reset)
+            r_BCD_Num_Sel <= 4'b1111;
+        else if (r_Segment_Count <= SEGMENT_NUM_USED - 1)
             r_BCD_Num_Sel <= i_BCD_Num[4*r_Segment_Count+3 -: 4];
         else
-            r_BCD_Num_Sel <= 4'b1111;
+            r_BCD_Num_Sel <= 4'b1110;
         
         r_Anodes <= r_Anodes << 1;
         r_Anodes[0] <= ~(r_Segment_Count == 0);
